2ec3e2029561f0cc97f5c6caff436ceabf1f3d6e
ailtonjunior11 <ailtonjunior.dev.frontend@gmail.com>
2025-03-12 07:18:26 -0300

    Atualização, função botão topo, script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+// ScrollReveal configurações
+const sr = ScrollReveal({ reset: true });
+
+// ScrollReveal configurações home
+sr.reveal(".conteudo-principal", {
+  duration: 1500,
+  delay: 550,
+  scale: 0.5,
+});
+
+sr.reveal(".img-principal", {
+  duration: 1500,
+  delay: 550,
+  scale: 0.7,
+});
+
+// ScrollReveal configurações projetos
+sr.reveal(".card-projeto-1", {
+  duration: 1500,
+  delay: 200,
+});
+
+sr.reveal(".card-projeto-2", {
+  duration: 1500,
+  delay: 300,
+});
+
+sr.reveal(".card-projeto-3", {
+  duration: 1500,
+  delay: 400,
+});
+
+sr.reveal(".card-projeto-4", {
+  duration: 1500,
+  delay: 450,
+});
+
+// ScrollReveal configurações sobre mim
+sr.reveal(".img-sobre", {
+  origin: "left",
+  distance: "350px",
+  delay: 50,
+  duration: 1500,
+  opacity: 0,
+});
+
+sr.reveal(".conteudo-sobre", {
+  origin: "right",
+  distance: "80px",
+  delay: 50,
+  duration: 1500,
+  opacity: 0,
+});
+
+sr.reveal(".botoes-contatos", {
+  origin: "bottom",
+  distance: "70px",
+  delay: 50,
+  duration: 1500,
+  opacity: 0,
+  scale: 1.1,
+});
+
+// Botão voltar ao topo configurações
+window.addEventListener("scroll", function () {
+  let scroll = document.querySelector(".btntopo");
+  scroll.classList.toggle("active", window.scrollY > 450);
+}); // Seleção e função de scroll do botão topo
+
+function backtopo() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+} // Função do botão voltar ao topo
+
+function voltartopo() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+} // Função da logo voltar ao topo
